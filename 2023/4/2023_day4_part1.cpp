fc4eb860f2538568660c5f7a8990cccfcc363a8b
Matt Logan <matt.b.logan@gmail.com>
2023-12-07 12:11:52 -0800

    Day 4

---
+++ 2023/4/2023_day4_part1.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_set>
+
+std::unordered_set<int> getNums(std::string str) {
+    std::unordered_set<int> set;
+    int curValue = 0;
+    for (int i = 0; i < str.length(); i++) {
+        if (std::isdigit(str[i])) {
+            curValue = curValue * 10 + str[i] - '0';
+        }
+        if (curValue > 0 && str[i] == ' ' || i == str.length() - 1) {
+            set.insert(curValue);
+            curValue = 0;
+        }
+    }
+    return set;
+}
+
+int main() {
+    std::ifstream file("2023/4/input.txt");
+
+    double sum = 0;
+
+    while (file) {
+        std::string line;
+        getline(file, line);
+        std::cout << line << "\n";
+        if (line.empty()) {
+            break;
+        }
+
+        int colonPos = line.find(':');
+        int barPos = line.find('|');
+
+        int winningNumsStrStart = colonPos + 2;
+        int winningNumsStrEnd = barPos - 1;
+        std::string winningNumsStr = line.substr(winningNumsStrStart, winningNumsStrEnd - winningNumsStrStart);
+        std::unordered_set<int> winningNums = getNums(winningNumsStr);
+
+        int myNumsStrStart = barPos + 2;
+        int myNumsStrEnd = line.length();
+        std::string myNumsStr = line.substr(myNumsStrStart, myNumsStrEnd - myNumsStrStart);
+        std::unordered_set<int> myNums = getNums(myNumsStr);
+
+        int count = 0;
+        for (int num: myNums) {
+            if (winningNums.count(num) > 0) {
+                count++;
+            }
+        }
+        if (count > 0) {
+            double value = std::pow(2, count - 1);
+            sum += value;
+        }
+    }
+
+    std::cout << "sum: " << sum;
+}
+
